53ecc9d817c027aa73a00747cc08b25b8a29231a
Aman <amantiwary161@gmail.com>
2025-12-08 02:46:46 +0530

    fonrtend started

---
+++ Brainly-Frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+import {Button} from "./componenents/ui/Buttons"
+import { PlusIcon } from './componenents/ui/icons/PlusIcon'
+
+function App() {
+  return (
+    <>
+      <Button startIcon = {<PlusIcon/>} size = "md" variant="Primary" text="Hello" />
+      <Button size = "lg" variant="Secondary" text="World" />
+    </>
+  )
+}
+
+export default App
---
+++ Brainly-Frontend/src/componenents/ui/Buttons.tsx
@@ -0,0 +1,32 @@
+import type { ReactElement } from "react";
+import type React from "react";
+
+export interface buttonProps {
+    variant: "Primary" | "Secondary" ,
+    size: "sm" | "md" | "lg",
+    text : string,
+    startIcon?: ReactElement,
+    endIcon?: ReactElement,
+    onClick?: () => void,
+}
+
+const Variantstyles = {
+    "Primary" : "bg-purple-600 text-white",
+    "Secondary" : "bg-purple-300 text-purple-600"
+}
+const sizeStyles = {
+  "sm": "py-1 px-2",
+  "md": "py-2 px-4",
+  "lg": "py-4 px-6",
+};
+
+const defaultStyles = "rounded-md flex"
+export const Button = (props: buttonProps) => {
+  return (
+    <button className={`${Variantstyles[props.variant]} ${sizeStyles[props.size]} ${defaultStyles}`}>
+      {props.startIcon ? <div className="pr-2"> {props.startIcon} </div> :null } {props.text} {props.endIcon}
+    </button>
+  );
+};
+
+
