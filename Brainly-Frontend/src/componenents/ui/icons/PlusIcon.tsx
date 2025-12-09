a374239c8f356f22ad1bc311a043ffc2d3bef450
Aman <amantiwary161@gmail.com>
2025-12-09 20:57:31 +0530

    updated frontend icons

--- Brainly-Frontend/src/componenents/ui/icons/PlusIcon.tsx
+++ Brainly-Frontend/src/componenents/ui/icons/PlusIcon.tsx
@@ -1,6 +1,9 @@
-export const PlusIcon = () => {
+import type { iconprops } from "./index";
+import { IconSizeVariants } from ".";
+
+export const PlusIcon = (props : iconprops) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={IconSizeVariants[props.size]}>
       <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
     </svg>
   );
